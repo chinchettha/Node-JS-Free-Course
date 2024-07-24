ff072eb02a4589f89180623cc359ed0f507f753f
chinchettha <152824848+chinchettha@users.noreply.github.com>
2024-07-24 13:59:47 +0700

    Node-JS-Free-Course

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
@@ -10,9 +10,9 @@ app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    debug("Listening on port:3000" + chalk.green(port));
+    debug("Listening on PORT : " + chalk.green(PORT));
 });
