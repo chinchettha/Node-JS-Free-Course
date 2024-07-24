23b53068bb388098527a5fa5e5a0cab166aaa3d2
chinchettha <152824848+chinchettha@users.noreply.github.com>
2024-07-24 13:36:19 +0700

    Node-JS-Free-Course

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const app = express(); // Call express to create an instance
+const app = express();
 const port = 3000;
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
     res.send('Hello World');
 });
 
 app.listen(port, () => {
-    debug("Listening on port" + chalk.green(port));
+    debug("Listening on port:3000" + chalk.green(port));
 });
