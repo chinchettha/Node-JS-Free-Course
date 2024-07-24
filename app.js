8e6bc8aa2576ce7064d2a1da3b9c28a9883d3bd2
chinchettha <152824848+chinchettha@users.noreply.github.com>
2024-07-24 16:10:06 +0700

    Node-JS-Free-Course

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const adminRouter = express.Router();
 
 app.set("views", path.join(__dirname, "./src/views")); // Use "views" instead of "view"
 app.set("view engine", "ejs");
@@ -12,7 +13,11 @@ app.set("view engine", "ejs");
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get("/admin")
+adminRouter.route("/").get((req, res) => {
+    res.send("Hello World !! I'm Admin.");
+});
+
+app.use("/admin", adminRouter)
 
 app.get("/", (req, res) => {
     res.render('index', { username : 'Chinchettha' , customer : ["น้องเอก", "คนนี้", "ดีที่หนึ่ง"] });
