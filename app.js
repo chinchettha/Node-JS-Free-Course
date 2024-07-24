8a68b3e3e7ef6f2ae4c5048beee6488652c1acf9
chinchettha <152824848+chinchettha@users.noreply.github.com>
2024-07-24 14:48:19 +0700

    Node-JS-Free-Course

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000; // Set a default port if PORT is not set in the environment
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
+app.set("views", path.join(__dirname, "./src/views")); // Use "views" instead of "view"
+app.set("view engine", "ejs");
+
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
-    res.send('Hello World!');
+    res.render('index', { username : 'Chinchettha' , customer : ["น้องเอก", "คนนี้", "ดีที่หนึ่ง"] });
 });
 
 app.listen(PORT, () => {
